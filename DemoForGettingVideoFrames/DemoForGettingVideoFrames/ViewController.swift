12b51b3d857153a5b3d4c1506263ca0dcd076b4f
bradleyyin <bradleyyin@gmail.com>
2019-09-23 10:42:30 -0700

    project set up and gitignore

---
+++ DemoForGettingVideoFrames/DemoForGettingVideoFrames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoForGettingVideoFrames
+//
+//  Created by Bradley Yin on 9/23/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
