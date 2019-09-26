320a29472bcde0b9f27436ccdf05b5139369f295
bradleyyin <bradleyyin@gmail.com>
2019-09-26 13:27:53 -0700

    add more tests

--- GifMaker/GifMakerTests/GifCollectionViewControllerTests.swift
+++ GifMaker/GifMakerTests/GifCollectionViewControllerTests.swift
@@ -15,45 +15,7 @@ class GifCollectionViewControllerTests: XCTestCase {
         let gifCollectionViewController = GifsCollectionViewController()
         XCTAssertTrue(gifCollectionViewController.gifController.gifs.count >= 0) 
     }
-    func testGenerateImages() {
-        let gifCollectionViewController = GifsCollectionViewController()
-        let url = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
-        let images = gifCollectionViewController.generateImages(url: url)
-        XCTAssertTrue(images.count == 901)
-    }
-    func testGenerateGifFromImages() {
-        let gifCollectionViewController = GifsCollectionViewController()
-        gifCollectionViewController.gifController.gifs = []
-        let url = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
-        let images = gifCollectionViewController.generateImages(url: url)
-        let didFinished = expectation(description: "didFinished")
-        gifCollectionViewController.generateGifFromImages(images: images, completion: {
-            didFinished.fulfill()
-        })
-        wait(for: [didFinished], timeout: 30)
-        XCTAssertEqual(1, gifCollectionViewController.gifController.gifs.count)
-    }
     
-    func testAnimatedGif() {
-        let gifCollectionViewController = GifsCollectionViewController()
-        gifCollectionViewController.gifController.gifs = []
-        let url = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
-        let images = gifCollectionViewController.generateImages(url: url)
-        let urlString = CGImage.animatedGif(from: images, fps: 15)!
-        XCTAssertTrue(urlString.contains("animated.gif"))
-    }
-    
-    func testGenerateGif() {
-        let gifCollectionViewController = GifsCollectionViewController()
-        gifCollectionViewController.gifController.gifs = []
-        let didFinished = expectation(description: "didFinished")
-        let url = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
-        gifCollectionViewController.generateGif(url: url) {
-            didFinished.fulfill()
-        }
-        wait(for: [didFinished], timeout: 30)
-        XCTAssertEqual(1, gifCollectionViewController.gifController.gifs.count)
-    }
     
    
 }
