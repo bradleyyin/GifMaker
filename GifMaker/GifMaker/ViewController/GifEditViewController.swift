2f40709fc3e7810162c75038a0ff737c4e6610aa
bradleyyin <bradleyyin@gmail.com>
2019-09-25 23:35:13 -0700

    remove text in image, causing too much performance issue delete gif if cancel out of the edit vc

--- GifMaker/GifMaker/ViewController/GifEditViewController.swift
+++ GifMaker/GifMaker/ViewController/GifEditViewController.swift
@@ -15,23 +15,21 @@ import ImageIO
 class GifEditViewController: UIViewController {
 
     @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var canvasView: UIView!
-    weak var textField: UITextField!
+    @IBOutlet weak var textField: UITextField!
     
     var assetURL: URL?
     let framesPerSecond = 15.0
     var gifController: GifController?
     var tempGifURL = ""
     var textFieldOrigin : CGPoint = CGPoint(x: 100, y: 100)
-    //var images: [CGImage]?
+    var finalTextFieldPoint: CGPoint = CGPoint.zero
+    
+    var images: [CGImage]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        print(assetURL)
         guard let url = assetURL else { return }
         generateGif(url: url) {
             guard let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
@@ -45,26 +43,27 @@ class GifEditViewController: UIViewController {
     
     func generateGif(url: URL, completion: @escaping () -> Void) {
         let images = generateImages(url: url)
-        generateGifFromImages(images: images, completion: {
+        self.images = images
+        generateGifFromImages(images: images, completion: { url in
+            self.tempGifURL = url
             completion()
         })
     }
     
-    func generateGifFromImages(images: [CGImage], completion: @escaping () -> Void) {
-        tempGifURL = CGImage.animatedGif(from: images, fps: self.framesPerSecond) ?? ""
-        print(tempGifURL)
-        completion()
-    }
-    
-    func createAndSaveNewGif() {
-        DispatchQueue.main.async {
-            self.gifController?.createNewGif(name: "", fileURL: self.tempGifURL,completion: {
-                self.deleteTempURL()
-            })
-        }
+    func generateGifFromImages(images: [CGImage], completion: @escaping (String) -> Void) {
+        let url = CGImage.animatedGif(from: images, fps: self.framesPerSecond) ?? ""
+        completion(url)
     }
     
     func deleteTempURL() {
+        guard let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        print(self.tempGifURL)
+        let fileURL: URL = dir.appendingPathComponent("\(self.tempGifURL)")
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+        } catch {
+            fatalError("unable to delete temp file")
+        }
         
     }
     
@@ -110,48 +109,96 @@ class GifEditViewController: UIViewController {
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let text = textField.text else { return }
+        gifController?.createNewGif(name: text, fileURL: self.tempGifURL, completion: {
+            self.dismiss(animated: true, completion: nil)
+        })
+        
     }
     @IBAction func cancelButtonTapped(_ sender: Any) {
         deleteTempURL()
+        self.dismiss(animated: true, completion: nil)
     }
-    @IBAction func addTextButtonTapped(_ sender: Any) {
-        let textField = UITextField(frame: CGRect(x: 100, y: 100, width: 100, height: 20))
-        textField.placeholder = "enter text"
-        textField.isUserInteractionEnabled = true
-        addTextButton.isHidden = true
-        textField.delegate = self
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(textFieldDrag(pan:)))
-        textField.addGestureRecognizer(gesture)
-        
-        textField.layer.borderWidth = 1
-        textField.layer.borderColor = UIColor.red.cgColor
-        self.textField = textField
-        canvasView.addSubview(textField)
-    }
+//    @IBAction func addTextButtonTapped(_ sender: Any) {
+//        let textField = UITextField(frame: CGRect(x: 100, y: 100, width: 200, height: 40))
+//        textField.placeholder = "enter text"
+//        textField.isUserInteractionEnabled = true
+//        addTextButton.isHidden = true
+//        textField.delegate = self
+//        let gesture = UIPanGestureRecognizer(target: self, action: #selector(textFieldDrag(pan:)))
+//        textField.addGestureRecognizer(gesture)
+//        textField.font = UIFont(name: "Helvetica", size: 50)
+//        self.textField = textField
+//        canvasView.addSubview(textField)
+//    }
     
-    @objc func textFieldDrag(pan: UIPanGestureRecognizer) {
-        print("Being Dragged")
-        if pan.state == .began {
-            print("panIF")
-            textFieldOrigin = pan.location(in: textField)
-            print(textFieldOrigin)
-        }else {
-            print("panELSE")
-            let location = pan.location(in: canvasView) // get pan location
-            textField.frame.origin = CGPoint(x: location.x - textFieldOrigin.x, y: location.y - textFieldOrigin.y)
-            print(textFieldOrigin)
-        }
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    @objc func textFieldDrag(pan: UIPanGestureRecognizer) {
+//        print("Being Dragged")
+//        if pan.state == .began {
+//            print("panIF")
+//            textFieldOrigin = pan.location(in: textField)
+//            print(textFieldOrigin)
+//        }else {
+//            print("panELSE")
+//            let location = pan.location(in: canvasView) // get pan location
+//            textField.frame.origin = CGPoint(x: location.x - textFieldOrigin.x, y: location.y - textFieldOrigin.y)
+//            finalTextFieldPoint = textField.frame.origin
+//            print(finalTextFieldPoint)
+//        }
+//    }
+//
+//    func textToImage(drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> CGImage? {
+//
+//        // Setup the font specific variables
+//        let textColor = UIColor.black
+//        let textFont = UIFont(name: "Helvetica", size: 50)!
+//
+//        // Setup the image context using the passed image
+//        let scale = UIScreen.main.scale
+//        UIGraphicsBeginImageContextWithOptions(inImage.size, false, scale)
+//
+//        // Setup the font attributes that will be later used to dictate how the text should be drawn
+//        let textFontAttributes = [
+//            NSAttributedString.Key.font: textFont,
+//            NSAttributedString.Key.foregroundColor: textColor,
+//        ]
+//
+//        // Put the image into a rectangle as large as the original image
+//        inImage.draw(in: CGRect(origin: CGPoint.zero, size: inImage.size))
+//
+//        // Create a point within the space that is as bit as the image
+//        let rect = CGRect(origin: atPoint, size: inImage.size)
+//
+//        // Draw the text into an image
+//        drawText.draw(in: rect, withAttributes: textFontAttributes)
+//
+//        // Create a new image out of the images we have created
+//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+//
+//        // End the context now that we have the image we need
+//        UIGraphicsEndImageContext()
+//
+//        //Pass the image back up to the caller
+//        let newCGImage = newImage?.cgImage
+//        return newCGImage
+//
+//    }
+//
+//    func generateGifWithText(completion: @escaping () -> Void) {
+//        guard let images = images, let text = textField.text else { return }
+//        var newImages: [CGImage] = []
+//        for image in images {
+//            let uiImage = UIImage(cgImage: image)
+//            guard let imageWithText = textToImage(drawText: text as NSString, inImage: uiImage, atPoint: CGPoint(x: finalTextFieldPoint.x + 20, y: finalTextFieldPoint.y)) else { continue }
+//            newImages.append(imageWithText)
+//        }
+//        generateGifFromImages(images: newImages) { url in
+//            self.createAndSaveNewGif(url: url, completion: {
+//                completion()
+//            })
+//
+//        }
+//    }
 
 }
 extension GifEditViewController: UITextFieldDelegate {
